e11122bac64d8a876dc293b51a6fd8887b4fe5df
Adams <adams.barajaa2014@gmail.com>
2026-07-05 11:21:04 -0500

    Commit1

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+import time
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    running = True
+    while running: pass
+
+
+
+    pygame.quit()
+
+main()
