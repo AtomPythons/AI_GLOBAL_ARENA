b467925df0cce17ab61d42897fa81791609d4b32
Adams <adams.barajaa2014@gmail.com>
2026-08-02 11:30:26 -0500

    ScreenPolish

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ SCREEN_HEIGHT = 500
 
 FPS = 60
 
-BACKGROUND_COLOR = (255, 0, 0)
+BACKGROUND_COLOR = (25, 30, 30)
+GREEN = (0, 255, 0)
+DARK_GREY = (60, 60, 70)
+
+LINE_COLOR = (200, 200, 200)
+
+
+def draw_world(screen):
+    screen.fill(BACKGROUND_COLOR)
+
+    pygame.draw.rect(screen, DARK_GREY, pygame.Rect(0, 0, 500, 450))
+
+    pygame.draw.line(screen, LINE_COLOR, (0, 450), (500, 450), 2)
 
 def main():
     # SOME CODE FROM DEVELOPER(IGOR)
@@ -25,7 +37,8 @@ def main():
                 running = False
             
                 
-        screen.fill((BACKGROUND_COLOR))
+        draw_world(screen)
+        
         clock.tick(FPS)
 
         pygame.display.update()
